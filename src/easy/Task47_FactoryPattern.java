bb2c94aa01e886d0a188c48af37d180a10bd0eb2
stoiandelev <stoian.delev@yahoo.com>
2022-11-20 10:50:10 +0200

    Factory Design Pattern for Java 8

---
+++ src/easy/Task47_FactoryPattern.java
@@ -0,0 +1,77 @@
+package easy;
+
+import java.security.Permission;
+import java.util.Scanner;
+
+public class Task47_FactoryPattern {
+
+    public static void main(String[] args) {
+        Do_Not_Terminate.forbidExit();
+
+        try{
+
+            Scanner sc=new Scanner(System.in);
+            //creating the factory
+            FoodFactory foodFactory = new FoodFactory();
+
+            //factory instantiates an object
+            Food food = foodFactory.getFood(sc.nextLine());
+
+
+            System.out.println("The factory returned "+food.getClass());
+            System.out.println(food.getType());
+        }
+        catch (Do_Not_Terminate.ExitTrappedException e) {
+            System.out.println("Unsuccessful Termination!!");
+        }
+    }
+
+}
+class Do_Not_Terminate {
+
+    public static class ExitTrappedException extends SecurityException {
+
+        private static final long serialVersionUID = 1L;
+    }
+
+    public static void forbidExit() {
+
+        //method is valid only for java 8, its terminate for java 17
+        final SecurityManager securityManager = new SecurityManager() {
+            @Override
+            public void checkPermission(Permission permission) {
+                if (permission.getName().contains("exitVM")) {
+                    throw new ExitTrappedException();
+                }
+            }
+        };
+        System.setSecurityManager(securityManager);
+    }
+}
+
+
+
+interface Food {
+    public String getType();
+}
+
+class Pizza implements Food {
+    @Override
+    public String getType() {
+        return "Someone ordered a Fast Food!";
+    }
+}
+
+class Cake implements Food {
+
+    @Override
+    public String getType() {
+        return "Someone ordered a Dessert!";
+    }
+}
+
+class FoodFactory {
+    public Food getFood(String order) {
+        return  order.equalsIgnoreCase("pizza") ? new Pizza() : new Cake();
+    }
+}
